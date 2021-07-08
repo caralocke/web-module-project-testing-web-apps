e0709ca89f22a47a886cdc1bb69f55f6e17cdd90
Cara Locke <caralocke@Caras-MBP.lan>
2021-07-08 16:53:06 -0400

    Successfully completed creating Test 3.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,35 +6,49 @@ import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
     //Arrange
-    render(<ContactForm/>)
+    render(<ContactForm/>) //Render the component.
 });
 
 test('renders the contact form header', ()=> {
     //Arrange
-    render(<ContactForm />)
+    render(<ContactForm />) //Render the component.
     //Act
-    const header = screen.getByText(/contact form/i)
+    const header = screen.getByText(/contact form/i) //Select the header of the ContactForm (the h1 in this case)
     //Assert
-    expect(header).toBeInTheDocument()
-    expect(header).toBeTruthy()
-    expect(header).toHaveTextContent(/contact form/i)
+    expect(header).toBeInTheDocument() //Assert if the header is in the document.
+    expect(header).toBeTruthy() //Assert if the header is truthy.
+    expect(header).toHaveTextContent(/contact form/i) //Assert if the header has the correct text content.
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
     //Arrange
-    render(<ContactForm/>)
+    render(<ContactForm/>) //render the component
     //Act
-    const firstNameInput = screen.getByLabelText(/first name*/i)
-    userEvent.type(firstNameInput, 'Cara')
+    const firstNameInput = screen.getByLabelText(/first name*/i) //Select the firstName input.
+    userEvent.type(firstNameInput, 'Cara') //Type into the first name input.
     
     //Assertions
-    expect(firstNameInput).toHaveValue('Cara')
-    const firstNameError = await screen.findByText(/error: firstName must have at least 5 characters/i)
-    expect(firstNameError).toBeInTheDocument()
+    expect(firstNameInput).toHaveValue('Cara') //Check the value of firstNameInput.
+    const firstNameError = await screen.findByText(/error: firstName must have at least 5 characters/i) //Wait until after the typing then select the error message.
+    expect(firstNameError).toBeInTheDocument() //Check that the error message appears.
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
+    //Arrange
+    render(<ContactForm/>) //Render the ContactForm.
+    //Act
+    const submitBtn = screen.getByRole('button') //Select the submit button.
+    userEvent.click(submitBtn) //Click the submit button
     
+    //After the button is clicked, select all three error messages.
+    const firstNameError = await screen.findByText(/error: firstName/i)
+    const lastNameError = await screen.findByText(/error: lastName/i)
+    const emailError = await screen.findByText(/error: email/i)
+    //Assertions
+    //Check fo all three error messages to be truthy.
+    expect(firstNameError).toBeTruthy()
+    expect(lastNameError).toBeTruthy()
+    expect(emailError).toBeTruthy()
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
